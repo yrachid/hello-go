d31c6b5fb39ac59f5535312dc7aa759580dfb270
yrachid <anuar.yasser@gmail.com>
2019-12-01 23:43:04 -0300

    Refactors unit tests

--- hello.go
+++ hello.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 const helloPrefix = "Hello, "
+const defaultHello = helloPrefix + "World"
 
 func Hello(name string) string {
+
+	if name == "" {
+		return defaultHello
+	}
+
 	return helloPrefix + name
 }
 
--- hello_test.go
+++ hello_test.go
@@ -4,11 +4,24 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	got := Hello("Doot")
-	expected := "Hello, Doot"
-
-	if got != expected {
-		t.Errorf("Got %q expected %q", got, expected)
-
+	isEqualTo := func(value string) string {
+		return value
 	}
+
+	assertThat := func(t *testing.T, actual, expected string) {
+		t.Helper()
+
+		if actual != expected {
+			t.Errorf("Got %q expected %q", actual, expected)
+		}
+	}
+
+	t.Run("Hello, Doot when Hello('Doot')", func(t *testing.T) {
+		assertThat(t, Hello("Doot"), isEqualTo("Hello, Doot"))
+	})
+
+	t.Run("Hello, World when Hello('')", func(t *testing.T) {
+		assertThat(t, Hello(""), isEqualTo("Hello, World"))
+	})
+
 }
